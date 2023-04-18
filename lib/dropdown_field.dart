c70d0727f1c77ee1c6487b8bfa535421ec448d11
army115 <89437364+army115@users.noreply.github.com>
2023-04-18 11:15:07 +0700

    Add files via upload

---
+++ lib/dropdown_field.dart
@@ -0,0 +1,139 @@
+part of 'custom_dropdown.dart';
+
+const _textFieldIcon = Icon(
+  Icons.keyboard_arrow_down_rounded,
+  // color: Colors.black,
+  size: 30,
+);
+const _contentPadding = EdgeInsets.only(left: 16);
+const _noTextStyle = TextStyle(height: 0);
+const _borderSide = BorderSide(color: Colors.transparent);
+const _errorBorderSide = BorderSide(color: Colors.redAccent, width: 2);
+
+class _DropDownField extends StatefulWidget {
+  final TextEditingController controller;
+  final VoidCallback onTap;
+  final Function(String)? onChanged;
+  final EdgeInsetsGeometry? contentPadding;
+  final String? hintText;
+  final TextStyle? hintStyle;
+  final TextStyle? style;
+  final String? errorText;
+  final TextStyle? errorStyle;
+  final InputBorder? focusedBorder;
+  final InputBorder? enabledBorder;
+  final InputBorder? focusedErrorBorder;
+  final InputBorder? errorBorder;
+  final BorderSide? borderSide;
+  final BorderSide? errorBorderSide;
+  final BorderRadius? borderRadius;
+  final Widget? suffixIcon;
+  final Widget? prefixIcon;
+  final Color? fillColor;
+
+  const _DropDownField({
+    Key? key,
+    required this.controller,
+    required this.onTap,
+    this.contentPadding,
+    this.onChanged,
+    this.suffixIcon,
+    this.prefixIcon,
+    this.focusedBorder,
+    this.enabledBorder,
+    this.focusedErrorBorder,
+    this.errorBorder,
+    this.hintText,
+    this.hintStyle,
+    this.style,
+    this.errorText,
+    this.errorStyle,
+    this.borderSide,
+    this.errorBorderSide,
+    this.borderRadius,
+    this.fillColor,
+  }) : super(key: key);
+
+  @override
+  State<_DropDownField> createState() => _DropDownFieldState();
+}
+
+class _DropDownFieldState extends State<_DropDownField> {
+  String? prevText;
+  bool listenChanges = true;
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.onChanged != null) {
+      widget.controller.addListener(listenItemChanges);
+    }
+  }
+
+  @override
+  void dispose() {
+    widget.controller.removeListener(listenItemChanges);
+    super.dispose();
+  }
+
+  @override
+  void didUpdateWidget(covariant _DropDownField oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (widget.onChanged != null) {
+      widget.controller.addListener(listenItemChanges);
+    } else {
+      listenChanges = false;
+    }
+  }
+
+  void listenItemChanges() {
+    if (listenChanges) {
+      final text = widget.controller.text;
+      if (prevText != null && prevText != text && text.isNotEmpty) {
+        widget.onChanged!(text);
+      }
+      prevText = text;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final border = OutlineInputBorder(
+      borderRadius: widget.borderRadius ?? BorderRadius.circular(12),
+      borderSide: widget.borderSide ?? _borderSide,
+    );
+
+    final errorBorder = OutlineInputBorder(
+      borderRadius: widget.borderRadius ?? BorderRadius.circular(12),
+      borderSide: widget.errorBorderSide ?? _errorBorderSide,
+    );
+
+    return TextFormField(
+      controller: widget.controller,
+      validator: (val) {
+        if (val?.isEmpty ?? false) return widget.errorText ?? '';
+        return null;
+      },
+      readOnly: true,
+      onTap: widget.onTap,
+      onChanged: widget.onChanged,
+      style: widget.style,
+      decoration: InputDecoration(
+        isDense: true,
+        contentPadding: widget.contentPadding,
+        prefixIcon: widget.prefixIcon,
+        suffixIcon: widget.suffixIcon ?? _textFieldIcon,
+        hintText: widget.hintText,
+        hintStyle: widget.hintStyle,
+        fillColor: widget.fillColor,
+        filled: true,
+        errorStyle: widget.errorText != null ? widget.errorStyle : _noTextStyle,
+        // border: border,
+        enabledBorder: widget.enabledBorder,
+        focusedBorder: widget.focusedBorder,
+        errorBorder: widget.errorBorder,
+        focusedErrorBorder: widget.focusedErrorBorder,
+      ),
+    );
+  }
+}
